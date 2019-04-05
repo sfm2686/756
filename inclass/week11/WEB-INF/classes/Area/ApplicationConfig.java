95217901c487f365b84f533edbbdbd8997dc9ebe
Sultan Mira <sultanmira@Sultans-MacBook-Pro.local>
2019-04-05 14:19:46 -0400

    Adding an example made during class for Java REST API

---
+++ inclass/week11/WEB-INF/classes/Area/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package Area;
+
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@javax.ws.rs.ApplicationPath("resource")
+public class ApplicationConfig extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    return getRestResourceClasses();
+  }
+
+  private Set<Class<?>> getRestResourceClasses() {
+    Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+    resources.add(Area.AreaCalculator.class);
+    return resources;
+  }
+}
